d21397f8ea13b235cc8915e21890d9f0670096b1
Jason Booth <hello@studiojasonbooth.com>
2019-05-12 19:42:11 +0200

    gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,93 @@
 
 
 
-var gulp = require('gulp')
-var sass = require('gulp-sass')
+var gulp = require("gulp")
+var sass = require("gulp-sass")
+// const sourcemaps = require('gulp-sourcemaps')
+var cleanCSS = require('gulp-clean-css')
+var autoprefixer = require('gulp-autoprefixer')
 
-var defaultTask = function () {
 
+var runSass = function () {
     return gulp.src("css/app.scss")
+    // .pipe(sourcemaps.init())
     .pipe(sass())
-    .pipe(gulp.dest("app.css"))
+
+    .pipe(
+        cleanCSS(
+        {
+            compatibility: 'ie8'
+        }
+    ))
+
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+
+    .pipe(gulp.dest("."))
+
 }
 
+
+var defaultTask = function () {
+    return gulp.parallel(runSass)
+}
+
+function watch(){
+  gulp.watch("css/app.scss", runSass)
+}
+
+
+
+exports.watch = watch
+exports.sass = runSass
 exports.default = defaultTask
+
+
+
+
+
+
+
+
+
+
+// var gulp = require('gulp')
+// var sass = require('gulp-sass')
+// var cleanCss = require('gulp-clean-css')
+// var sourcemaps = require('gulp-sourcemaps')
+// var autoprefixer = require('gulp-autoprefixer')
+ 
+// var runSass = function (){
+//     return gulp.src("css/app.scss")
+//     .pipe(sourcemaps.init())
+//     .pipe(sass())
+//     .pipe(autoprefixer({
+//       browsers: ['last 2 versions'],
+//       cascade: false
+//     }))
+//     .pipe(
+//       cleanCss({
+//         compatibility: 'ie8'
+//       })
+//     )
+//     .pipe(sourcemaps.write())
+//     .pipe(gulp.dest("."))
+
+// }
+
+
+// function watch(){
+//   gulp.watch("css/app.scss", runSass)
+// }
+
+
+
+// var defaultTask = function () {
+//   return gulp.parallel(runSass)
+// }
+
+// exports.watch = watch
+// exports.sass = runSass
+// exports.default = defaultTask
